c73c7f80de104b95bdffc2fb068bf3f7fce47a51
WolfgangPrath <wolfgang.prath@outlook.com>
2018-11-06 22:01:22 +0100

    Initial commit

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,56 @@
+package com.company;
+
+public class HelloWorld {
+    //Hauptprogramm ist immer main
+
+    public static void unterprogramm()
+    {
+        System.out.println();
+        System.out.println("Mein Unterprogramm");
+        System.out.println();
+
+        boolean istSamstag = true;
+        boolean istFH = true;
+
+        if (istSamstag == true && istFH)
+        {
+            System.out.println("Juhu, ich bin am Campus02!");
+        }
+        else
+        {
+            System.out.println("Ich lerne gerne zuhause Java :-)");
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        int maxAusgabe;
+        maxAusgabe = 0;
+
+        while (maxAusgabe< 100) {
+            System.out.println("Hallo Welt " + maxAusgabe);
+            maxAusgabe = maxAusgabe + 1;
+        }
+
+        long startwert = 31102018;
+        unterprogramm();
+        int laufWert;
+        laufWert = 2;
+
+        while (laufWert <= 9)
+        {
+            startwert = startwert * laufWert;
+            laufWert = laufWert + 1;
+            System.out.println(startwert);
+        }
+
+        laufWert = 2;
+        while (laufWert <=9)
+        {
+                startwert = startwert / laufWert;
+                laufWert = laufWert + 1;
+                System.out.println(startwert);
+        }
+    }
+}
